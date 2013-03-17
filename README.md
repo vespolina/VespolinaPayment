1d2da02a50216c6952b539f16a7e5f3b4f3c9df3
iampersistent <develop@zestic.com>
2013-03-17 02:33:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+VespolinaPayment
+================
+[![Build Status](https://travis-ci.org/vespolina/VespolinaPayment.png?branch=master)](https://travis-ci.org/vespolina/VespolinaPayment)
+
+
---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2013 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
